ecd0d1e46c23f6e4560130c9d97783d6b899846c
panmingjie <1430001300@qq.com>
2018-06-08 01:13:21 +0800

    添加zookeeper依赖

---
+++ src/test/java/zookeeperTest.java
@@ -0,0 +1,11 @@
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Test;
+
+public class zookeeperTest {
+
+
+    @Test
+    public void testCon(){
+        ZooKeeper zookeeper = new ZooKeeper();
+    }
+}
